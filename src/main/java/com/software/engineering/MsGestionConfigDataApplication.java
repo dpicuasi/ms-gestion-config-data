3fa969fb3009fe4e5aeb024beace3e9f10701587
Daniel Picuasi <danyeledg@gmail.com>
2022-10-15 01:13:01 -0500

    Archivos agregados

---
+++ src/main/java/com/software/engineering/MsGestionConfigDataApplication.java
@@ -0,0 +1,13 @@
+package com.software.engineering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGestionConfigDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGestionConfigDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/software/engineering/MsGestionConfigDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.engineering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGestionConfigDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
